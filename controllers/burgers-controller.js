f8fb6504a401e309a67189106e47a138965d6860
Jalia Evans <irohdesign@outlook.com>
2019-01-21 08:29:16 -0500

    working on routes

--- controllers/burgers-controller.js
+++ controllers/burgers-controller.js
@@ -4,7 +4,12 @@ const burger = require("../models/burger.js");
 // routes
 module.exports = function(app) {
     // show all burgers - app.get
-
+    app.get("/", function(req, res) {
+        burger.selectAll()
+        .then(function(data) {
+            console.log(data);
+        })
+    })
     
     // create new burger - app.post
 

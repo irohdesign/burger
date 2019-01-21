b1a655b0fecfddf1f7eb828893e7c639b5da9fda
Jalia Evans <irohdesign@outlook.com>
2019-01-20 20:21:10 -0500

    added selectAll and insertOne functions with promises

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,33 @@
 const connection = require("./connection.js");
 
 function ORM(table) {
+    this.table = table;
+
     // select all
+    this.selectAll = function() {
+        const sql = `SELECT * FROM ?`;
+
+        return new Promise(function(resolve, reject) {
+            connection.query(sql, table, function(err, data) {
+                if(err) reject(err);
+                resolve(data);
+            });
+        })
+    },
 
     // insertOne
+    this.insertOne = function(burger_name) {
+        const sql = `INSERT INTO ?? (burger_name) VALUES ?`;
+
+        return new Promise(function(resolve, reject) {
+            connection.query(sql, [table, burger_name], function(err, data) {
+                if(err) reject(err);
+                resolve(data);
+            });
+        })
+    }
 
     // updateOne
 }
+
+module.exports = ORM;
